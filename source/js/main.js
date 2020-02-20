17cad02110131e6d592d522f9296393805558f5b
Alexey <mistesqr@gmail.com>
2020-02-20 21:58:47 +0200

    dummy and last fixes

--- source/js/main.js
+++ source/js/main.js
@@ -1,14 +1,17 @@
 
 var hamburger = document.querySelector(".hamburger");
 var submenu = document.querySelector(".submenu");
+var body = document.querySelector("body");
+var dummy = document.querySelector(".dummy");
 // On click
 hamburger.addEventListener("click", function() {
   // Toggle class "is-active"
   hamburger.classList.toggle("is-active");
   submenu.classList.toggle("submenu--active");
+  body.classList.toggle("ov-hidden");
 });
 
-var time = 1582231568;
+var time = 1616096925;
 
  // Set up FlipDown
  var flipdown = new FlipDown(time)
@@ -18,5 +21,13 @@ var time = 1582231568;
 
    // Do something when the countdown ends
    .ifEnded(() => {
+     startStub();
      console.log('The countdown has ended!');
    });
+
+
+   function startStub() {
+      body.classList.add("ov-hidden");
+      dummy.classList.add("dummy--active");
+      window.scrollTo(0, 0);
+  };
